31eefdc9c5606fa1045a235aebbe569f7bb68d3f
Manuel Ambriz <manuelambriz@MacBook-Air-de-Manuel.local>
2018-01-17 18:28:40 -0600

    Initial Commit

--- routers_userapp.js
+++ routers_userapp.js
@@ -559,7 +559,7 @@ router.route("/notificaciones/:id")
         .post(function(req,res){ 
         var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
         var f=new Date();
-        var fecha = f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear() + f.getHours + ":" + f.getMinutes
+        var fecha = f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear() + " " + f.getHours() + ":" + f.getMinutes()
         var notificaciones = new Notificaciones ({ titulo: req.body.titulo , user_id: res.locals.user._id, redcamaras_id: req.params.id, mensaje: req.body.mensaje, fecha: fecha
          });
     
@@ -721,7 +721,7 @@ router.route("/sensores/:id")
         .post(function(req,res){
         var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
         var f=new Date();
-        var fecha = f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear()
+        var fecha = f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear() + " " + f.getHours() + ":" + f.getMinutes()
         var notificaciones = new Notificaciones ({ titulo: req.body.titulo , user_id: res.locals.user._id, redcamaras_id: req.params.id, mensaje: req.body.mensaje, fecha: fecha
          });
     
